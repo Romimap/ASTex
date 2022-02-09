9100d362ab294b22cc3e487deb42aa5cc83252ae
Romain Fournier <romain.fournier.095@gmail.com>
2022-02-09 15:24:38 +0100

    removed hazardous PSD implementation

--- Test/test_fourierTransform.cpp
+++ Test/test_fourierTransform.cpp
@@ -5,18 +5,6 @@
 
 using namespace ASTex;
 
-//Wrong (?)
-void computePSD (const ImageSpectrald modulus, const ImageSpectrald phase, ImageSpectrald& psd) {
-    for (int x = 0; x < modulus.width(); x++) {
-        for (int y = 0; y < modulus.width(); y++) {
-            double m = modulus.pixelAbsolute(x, y);
-            double p = phase.pixelAbsolute(x, y);
-            long double j = m*m + p*p;
-            psd.pixelAbsolute(x, y) = std::min((long double)std::numeric_limits<double>::max(), std::max((long double)0.0, j));
-        }
-    }
-}
-
 int main (int argc, char* argv[]) {
     if (argc != 3) {
         printf("usage: <input file> <output file>");
@@ -31,16 +19,13 @@ int main (int argc, char* argv[]) {
     ImageSpectrald phase;
     Fourier::fftForwardModulusAndPhase(input, modulus, phase, true);
 
-    ImageSpectrald psd(modulus.width());
-    computePSD(modulus, phase, psd);
-
-    ImageGrayu8 outputM(psd.width(), psd.height());
+    ImageGrayu8 outputM(modulus.width(), modulus.height());
     {   
         double max = 0;
-        for (int x = 0; x < psd.width(); x++) {
-            for (int y = 0; y < psd.height(); y++) {
-                max = std::max(max, psd.pixelAbsolute(x, y));
-                outputM.pixelAbsolute(x, y) = tanh(psd.pixelAbsolute(x, y) * 0.000000001) * 255.0;
+        for (int x = 0; x < modulus.width(); x++) {
+            for (int y = 0; y < modulus.height(); y++) {
+                max = std::max(max, modulus.pixelAbsolute(x, y));
+                outputM.pixelAbsolute(x, y) = tanh(modulus.pixelAbsolute(x, y) * 0.000000001) * 255.0;
             }
         }
         std::cout << max << std::endl;
@@ -48,7 +33,7 @@ int main (int argc, char* argv[]) {
     outputM.save(argv[2]);
 
     HistogramSpectrald h;
-    h.compute(psd);
+    h.compute(modulus);
 
     std::cout << h.mean() << "  " << h.variance() << std::endl;
 
